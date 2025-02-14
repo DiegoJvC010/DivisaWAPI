189c4324752dcd6ccbcb9e383dfd9686e9dc424e
EduardoCM22 <98827037+EduardoCM22@users.noreply.github.com>
2025-02-13 19:41:21 -0600

    Se agregaron los Convertes para la BD

--- app/src/main/java/com/example/divisawapi/data/Converters.kt
+++ app/src/main/java/com/example/divisawapi/data/Converters.kt
@@ -1,4 +1,16 @@
 package com.example.divisawapi.data
 
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 class Converters {
+    @TypeConverter
+    fun fromRatesMap(value: Map<String, Double>): String = Gson().toJson(value)
+
+    @TypeConverter
+    fun toRatesMap(value: String): Map<String, Double> {
+        val mapType = object : TypeToken<Map<String, Double>>() {}.type
+        return Gson().fromJson(value, mapType)
+    }
 }
