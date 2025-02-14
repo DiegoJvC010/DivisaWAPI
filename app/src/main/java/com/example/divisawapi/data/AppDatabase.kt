551df5ba10e6f78783cc497500603c7d83a2d211
EduardoCM22 <98827037+EduardoCM22@users.noreply.github.com>
2025-02-13 19:24:26 -0600

    Codigo de BD

--- app/src/main/java/com/example/divisawapi/data/AppDatabase.kt
+++ app/src/main/java/com/example/divisawapi/data/AppDatabase.kt
@@ -1,2 +1,25 @@
 package com.example.divisawapi.data
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ExchangeRateEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun exchangeRateDao(): ExchangeRateDao
+
+    companion object {
+        @Volatile private var instance: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase =
+            instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "exchange_rate_database"
+                ).build().also { instance = it }
+            }
+    }
+}
