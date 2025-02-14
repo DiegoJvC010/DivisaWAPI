50751a3b2b5737c349f980f50397430bab6f0d85
edwcm <none>
2025-02-13 19:46:20 -0600

    Se agrego el DAO para la BD

--- app/src/main/java/com/example/divisawapi/data/ExchangeRateDao.kt
+++ app/src/main/java/com/example/divisawapi/data/ExchangeRateDao.kt
@@ -1,4 +1,15 @@
 package com.example.divisawapi.data
 
-class ExchangeRateDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ExchangeRateDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(exchangeRate: ExchangeRateEntity)
+
+    @Query("SELECT * FROM exchange_rates ORDER BY id DESC")
+    suspend fun getAllRates(): List<ExchangeRateEntity>
 }
