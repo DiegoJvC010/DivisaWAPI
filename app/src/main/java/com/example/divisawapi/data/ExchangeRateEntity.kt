58a739f1de05724a8cfa416f0d6095d3bca31726
edwcm <none>
2025-02-13 19:47:46 -0600

    Se creo la entidad/objeto que almacenara los datos de la API

--- app/src/main/java/com/example/divisawapi/data/ExchangeRateEntity.kt
+++ app/src/main/java/com/example/divisawapi/data/ExchangeRateEntity.kt
@@ -1,4 +1,13 @@
 package com.example.divisawapi.data
 
-class ExchangeRateEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "exchange_rates")
+data class ExchangeRateEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val timeLastUpdateUtc: String,
+    val timeNextUpdateUtc: String,
+    val rates: Map<String, Double>
+)
