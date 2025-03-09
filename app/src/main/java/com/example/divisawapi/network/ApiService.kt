f74c1eff494a5be6970b0258ef555bb6c7870718
Diego Jesus <s21120187@alumnos.itsur.edu.mx>
2025-03-09 13:26:51 -0600

    Ahora si API pro de actualizacion cada 60 minutos

--- app/src/main/java/com/example/divisawapi/network/ApiService.kt
+++ app/src/main/java/com/example/divisawapi/network/ApiService.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiService{
-    @GET("v6/21979ca005773ce125edf08e/latest/MXN")
+    @GET("v6/ea89034455f0918eb70bb598/latest/MXN")
     suspend fun getExchangeRates(): Response<ExchangeRateResponse>
 }
